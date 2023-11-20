5e5d693015af3601bfe15b25c55960b24c21f428
Nehal Ingole <nehal712521@gmail.com>
2023-11-20 14:49:57 +0530

    Learning

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import KgButton from "./KgButton";
+
+KgButton
+
+function App() {
+  return (<div>
+
+    <h1>
+      hello everyone 
+    </h1>
+    <KgButton></KgButton>
+    
+  </div>)
+  
+
+  
+}
+
+export default App;
